1f5507b46c09d1cc5ed524dd0d50f547f8735c33
Shaikat Majumder <68814937+shaikat010@users.noreply.github.com>
2022-12-29 16:07:50 +0600

    Create Subcurrency.sol

---
+++ Subcurrency.sol
@@ -0,0 +1,54 @@
+// from official solidity documentation
+// SPDX-License-Identifier: GPL-3.0
+
+// Solidity program 
+// to store 
+// Employee Details
+pragma solidity >= 0.7.0 <0.9.0;
+  
+// Creating a Smart Contract
+contract EmployeeDetails{
+  
+   // Structure of employee
+   struct Employee{
+       
+       // State variables
+       int empid;
+       string name;
+       string department;
+       string designation;
+   }
+   
+   Employee []emps;
+  
+   // Function to add 
+   // employee details
+   function addEmployee(int empid, string memory name, string memory department, string memory designation) public{
+       Employee memory e =Employee(empid,name,department,designation);
+       emps.push(e);
+   }
+  
+  // Function to get
+  // details of employee
+   function getEmployee(int empid) public view returns(string memory, string memory, string memory){
+       uint i;
+
+       for(i=0;i<emps.length;i++)
+       {
+           Employee memory e =emps[i];
+           
+           // Looks for a matching 
+           // employee id
+           if(e.empid==empid)
+           {
+                  return(e.name,e.department,e.designation);
+           }
+       }
+       
+     // If provided employee 
+     // id is not present
+     // it returns Not 
+     // Found
+     return("Not Found", "Not Found", "Not Found");
+   }
+}
